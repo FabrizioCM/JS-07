7225354fb8f8511d769784b00fdfae5848b36a91
FabrizioCM <paulofabriziocm@gmail.com>
2023-09-27 23:14:26 -0600

    revision nueva agregue advertencia del while esta peligroso

--- JavaScript/fetch.js
+++ JavaScript/fetch.js
@@ -33,10 +33,10 @@ document.getElementById('get-btn') //Obtenemos la info del id del boton
     .addEventListener('click', async ()=> { // ponemos un evento que escuchara el click y ejecutara una funcion async 
         const text = document.getElementById('poke-name').value.toLowerCase();
         const pokemon = await fetchPokemon(text); // definimos const pokemon y esperamos al fetchPokemon
-        localStorage.setItem('currentPokeID', pokemon.id);
+        localStorage.setItem('currentPokeID', pokemon.id); //almaceno en el local storage
         //console.log(pokemon.id); // imprimimos pokemon
         console.log(pokemon.name);
-        tarjetaPokemon(pokemon);
+        tarjetaPokemon(pokemon);// llamo a la funcion tarjetaPokemon para que haga una tarjeta
     })
 
     
@@ -54,60 +54,59 @@ document.addEventListener('DOMcontentLoaded', async ()=> {
 //Obtener el siguiente
 
 
-document.getElementById('previous-btn')
-    .addEventListener('click', async () => {
+document.getElementById('previous-btn') // obtenemos info del elemento previous-btn
+    .addEventListener('click', async () => { //añadimos un evento asincrono mediante un click
         const currentPokeID = parseInt(localStorage.getItem('currentPokeID'));
-        const newID = (currentPokeID -1);
-        const pokemon = await fetchPokemon(newID);
-        console.log(pokemon.name);
-        localStorage.setItem('currentPokeID', newID);
-        tarjetaPokemon(pokemon);
+        const newID = (currentPokeID -1);//retrocedo un lugar en el ID
+        const pokemon = await fetchPokemon(newID);  //espera la promesa 
+        console.log(pokemon.name); //imprimo en consola el nombre del pokemon
+        localStorage.setItem('currentPokeID', newID); //Actualiza el local storage
+        tarjetaPokemon(pokemon);   // llamo a la funcion tarjetaPokemon para que haga una tarjeta
     });
 
     document.getElementById('next-btn')
     .addEventListener('click', async ()=>{
         const currentPokeID = parseInt(localStorage.getItem('currentPokeID'));
-        const newID =currentPokeID +1;
-        const pokemon = await fetchPokemon(newID);
-        console.log(pokemon.name);
-        localStorage.setItem('currentPokeID', newID); 
-        tarjetaPokemon(pokemon);
+        const newID =currentPokeID +1; //retrocedo un lugar en el ID
+        const pokemon = await fetchPokemon(newID); //espera la promesa 
+        console.log(pokemon.name);//imprimo en consola el nombre del pokemon
+        localStorage.setItem('currentPokeID', newID); //Actualiza el local storage
+        tarjetaPokemon(pokemon); // llamo a la funcion tarjetaPokemon para que haga una tarjeta
     })
 
 
 ///////
 
-function tarjetaPokemon(pokemon) {
-    const card = document.getElementById('perfilPokemon');
+function tarjetaPokemon(pokemon) { // creo funcion tarjetaPokemon
+    const card = document.getElementById('perfilPokemon'); // obtengo el elemento desde el div perfilPokemon del HTML
 
-    while (card.firstChild) {
-        card.removeChild(card.firstChild);
+    while (card.firstChild) { //con un while reviso si el elemento tarjeta tiene un hijo usando el first child 
+        card.removeChild(card.firstChild); // si en efecto tiene un hijo lo borra (aqui tengo un error bueno no es error ya que hace lo que pido que es borrar todo así que si se das dos clicks se borra hasta el almacenamiento local )
     }
-
-
+    //Aqui me traigo todos los elementos desde el nombre, id y el peso
     const nombrePokemon = document.createElement('h2');
     nombrePokemon.textContent = `Nombre: ${pokemon.name}`;
     const idPokemon = document.createElement('p');
     idPokemon.textContent = `ID: ${pokemon.id}`;
     const pesoPokemon = document.createElement('p');
     pesoPokemon.textContent = `Peso: ${pokemon.weight}`;
-
+//los almaceno localmente para cuando refrescas la pagina no hacer dos veces ppr que el while lo elimina :) lo voy a corregir 
     localStorage.setItem('currentPokemonName', pokemon.name);
     localStorage.setItem('currentPokemonID', pokemon.id);
     localStorage.setItem('currentPokemonWeight', pokemon.weight);
-
+//agrego nuevos elementos a la tarjeta
     card.appendChild(nombrePokemon);
     card.appendChild(idPokemon);
     card.appendChild(pesoPokemon);
     
 }
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener('DOMContentLoaded', async () => { //Abro una funcion asincrona para cuando el 
     const pokemon = {
-        id: parseInt(localStorage.getItem('currentPokemonID')) || 1,
-        name: localStorage.getItem('currentPokemonName') || '',
-        weight: localStorage.getItem('currentPokemonWeight') || ''
+        id: parseInt(localStorage.getItem('currentPokemonID')) || 1, //Obtengo la informacion del localStorage
+        name: localStorage.getItem('currentPokemonName') || '', //Obtengo la informacion del localStorage
+        weight: localStorage.getItem('currentPokemonWeight') || '' //Obtengo la informacion del localStorage
     };
-    tarjetaPokemon(pokemon);
+    tarjetaPokemon(pokemon);  // con esto llamo a la funcion para mostrar la targeta desde el almacenamiento  
 });
 
 
